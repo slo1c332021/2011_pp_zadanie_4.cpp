c0d812819961ee6d6e6970ea2efed8742c9aa543
slo1c332021 <82053050+slo1c332021@users.noreply.github.com>
2022-03-24 10:17:26 +0100

    Create 2011_pp_zadanie_4.cpp

---
+++ 2011_pp_zadanie_4.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int licznik = 0;
+    string s;
+    ifstream f("hasla.txt");
+    while(f>>s){
+            if(s.size)()%2==0) licznik++;
+        
+    }
+cout << "Odp. a =" << licznik<< "\n";
+}
